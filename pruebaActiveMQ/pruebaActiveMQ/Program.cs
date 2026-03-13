cda44826992aa43c287ef8c8a57a335dac93a046
Marvin Gudiel <mgudiell@miumg.edu.gt>
2026-03-12 20:29:39 -0500

    subiendo Prueba de concepto mensajeria con active mq producer-consumer

---
+++ pruebaActiveMQ/pruebaActiveMQ/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string direccionBroker = "activemq:tcp://localhost:61616";
+
+        IConnectionFactory fabricaConexion = new ConnectionFactory(direccionBroker);
+
+        using (IConnection conexion = fabricaConexion.CreateConnection())
+        {
+            conexion.Start();
+
+            using (ISession sesion = conexion.CreateSession())
+            {
+                IDestination colaDestino = SessionUtil.GetDestination(sesion, "queue://cola.demo");
+
+                using (IMessageProducer productor = sesion.CreateProducer(colaDestino))
+                {
+                    ITextMessage mensaje = sesion.CreateTextMessage("Usando ActiveMQ desde C#");
+
+                    productor.Send(mensaje);
+
+                    Console.WriteLine("Mensaje enviado correctamente.");
+                }
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ receptorMensajes/receptorMensajes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string direccionBroker = "activemq:tcp://localhost:61616";
+
+        IConnectionFactory fabricaConexion = new ConnectionFactory(direccionBroker);
+
+        using (IConnection conexion = fabricaConexion.CreateConnection())
+        {
+            conexion.Start();
+
+            using (ISession sesion = conexion.CreateSession())
+            {
+                IDestination colaDestino = SessionUtil.GetDestination(sesion, "queue://cola.demo");
+
+                using (IMessageConsumer consumidor = sesion.CreateConsumer(colaDestino))
+                {
+                    Console.WriteLine("Esperando mensaje...");
+
+                    ITextMessage mensajeRecibido = consumidor.Receive() as ITextMessage;
+
+                    if (mensajeRecibido != null)
+                    {
+                        Console.WriteLine("Mensaje recibido:");
+                        Console.WriteLine(mensajeRecibido.Text);
+                    }
+                }
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
